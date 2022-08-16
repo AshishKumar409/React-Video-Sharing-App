b87e4aa8128ac8b78a49bd492c4b087bd66eb980
Ashish Kumar Nath <ashishkumarnath9@gmail.com>
2022-08-16 08:44:24 +0000

    created most of the main feature

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,119 @@
-export const Menu=()=>{
-  return <div>menu</div>
+import styled from "styled-components"
+import MyLogo from '../img/Logo.png'
+import {IconsAndNames1,IconsAndNames2,IconsAndNames3,IconsAndNames4} from '../icons/icons.jsx'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
+const Container = styled.div`
+    flex:1;
+    background-color:#202020;
+    height:100vh;
+    color:white;
+    font-size:14px;
+/*     overflow:hidden;
+
+   :hover{
+     overflow:auto;
+   } */
+   
+  `
+
+const Wrapper = styled.div`
+    padding:18px 26px;
+    
+  `
+
+const Logo = styled.div`
+   display:flex;
+   align-items:center;
+   gap:5px;
+  font-weight:bold;
+  margin-bottom:25px;
+  `
+
+const Img = styled.img` 
+  height:25px;
+  `
+
+const Item = styled.div`
+   display:flex;
+   align-items:center;
+  gap:20px;
+  cursor:pointer;
+  padding:7.5px 0px
+  
+  `
+
+const Hr = styled.hr`
+  margin:15px 0px;
+  border:0.5px solid #373737;
+  `
+
+const Login = styled.div``
+const Button = styled.button`
+  display:flex;
+  align-items:center;
+  gap:2px;
+  padding:5px 15px;
+  background-color:transparent;
+  border:1px solid #3ea6ff;
+  color:#3ea6ff;
+  border-radius:3px;
+  font-weight:500;
+  margin-top:10px;
+  cursor:pointer;
+  `
+
+export const Menu = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={MyLogo} />
+          YouTube
+        </Logo>
+
+        {
+          IconsAndNames1.map((obj)=>{
+            return <Item key={obj.name}>
+              {obj.icon}
+              {obj.name}
+            </Item>
+          })
+        }
+        <Hr/>
+        {
+          IconsAndNames2.map((obj)=>{
+            return <Item key={obj.name}>
+              {obj.icon}
+              {obj.name}
+            </Item>
+          })
+        }
+        <Hr/>
+        <Login>
+           Sign in to like videos, comment, and subscribe
+          <Button> <AccountCircleOutlinedIcon/>SIGN IN</Button>
+        </Login>
+        <Hr/>
+        {
+          IconsAndNames3.map((obj)=>{
+            return <Item key={obj.name}>
+              {obj.icon}
+              {obj.name}
+            </Item>
+          })
+        }
+        <Hr/>
+        {
+          IconsAndNames4.map((obj)=>{
+            return <Item key={obj.name}>
+              {obj.icon}
+              {obj.name}
+            </Item>
+          })
+        }
+
+        
+      </Wrapper>
+    </Container>)
 }
---
+++ src/icons/icons.jsx
@@ -0,0 +1,90 @@
+import HomeIcon from '@mui/icons-material/Home';
+import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined';
+import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
+import LibraryMusicOutlinedIcon from '@mui/icons-material/LibraryMusicOutlined';
+import SportsBasketballOutlinedIcon from '@mui/icons-material/SportsBasketballOutlined';
+import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
+import MovieCreationOutlinedIcon from '@mui/icons-material/MovieCreationOutlined';
+import NewspaperOutlinedIcon from '@mui/icons-material/NewspaperOutlined';
+import LiveTvOutlinedIcon from '@mui/icons-material/LiveTvOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined';
+
+
+
+
+export let IconsAndNames1=[
+  {
+    icon:<HomeIcon/>,
+    name:"Home"
+  },
+  {
+    icon:<ExploreOutlinedIcon/>,
+    name:"Explore"
+  },
+  {
+    icon:<SubscriptionsOutlinedIcon/>,
+    name:"Subscriptions"
+  }
+]
+
+export let IconsAndNames2=[
+  {
+    icon:<VideoLibraryOutlinedIcon/>,
+    name:"Library"
+  },
+  {
+    icon:<HistoryOutlinedIcon/>,
+    name:"History"
+  }
+]
+
+export let IconsAndNames3=[
+  {
+    icon:<LibraryMusicOutlinedIcon/>,
+    name:"Music"
+  },
+  {
+    icon:<SportsBasketballOutlinedIcon/>,
+    name:"Sports"
+  },
+  {
+    icon:<SportsEsportsOutlinedIcon/>,
+    name:"Gaming"
+  },
+  {
+    icon:<MovieCreationOutlinedIcon/>,
+    name:"Movies"
+  },
+  {
+    icon:<NewspaperOutlinedIcon/>,
+    name:"News"
+  },
+  {
+    icon:<LiveTvOutlinedIcon/>,
+    name:"Live"
+  },
+]
+
+export let IconsAndNames4=[
+  {
+    icon:<SettingsOutlinedIcon/>,
+    name:"Settings"
+  },
+  {
+    icon:<FlagOutlinedIcon/>,
+    name:"Report"
+  },
+  {
+    icon:<HelpOutlineOutlinedIcon/>,
+    name:"Help"
+  },
+  {
+    icon:<SettingsBrightnessOutlinedIcon/>,
+    name:"Light Mode"
+  }
+]
